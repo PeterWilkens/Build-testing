6d55845d5c3f3ec6c6b5e795450c9418c9e7def0
PeterWilkens <p.j.wilkens@btinternet.com>
2026-03-08 14:15:12 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -10,112 +10,13 @@ var ptx_lunr_docs = [
   "body": "  "
 },
 {
-  "id": "sec-section-name",
+  "id": "ch-chapter-title",
   "level": "1",
-  "url": "sec-section-name.html",
-  "type": "Section",
-  "number": "1.1",
-  "title": "C System Treble Keyboard Layout Diagram",
-  "body": " C System Treble Keyboard Layout Diagram   Treble Side Keyboard. Vertical lines represent Bellows      "
-},
-{
-  "id": "tikz-example",
-  "level": "2",
-  "url": "sec-section-name.html#tikz-example",
-  "type": "Figure",
-  "number": "1.1.1",
-  "title": "",
-  "body": " Treble Side Keyboard. Vertical lines represent Bellows     "
-},
-{
-  "id": "sec-free-bass-keyboard-layout",
-  "level": "1",
-  "url": "sec-free-bass-keyboard-layout.html",
-  "type": "Section",
-  "number": "1.2",
-  "title": "C System Free Bass Keyboard Layout Diagram",
-  "body": " C System Free Bass Keyboard Layout Diagram   Free Bass Keyboard. Vertical lines represent Bellows      "
-},
-{
-  "id": "freebass",
-  "level": "2",
-  "url": "sec-free-bass-keyboard-layout.html#freebass",
-  "type": "Figure",
-  "number": "1.2.1",
-  "title": "",
-  "body": " Free Bass Keyboard. Vertical lines represent Bellows     "
-},
-{
-  "id": "sec-stradella-bass-keyboard-layout",
-  "level": "1",
-  "url": "sec-stradella-bass-keyboard-layout.html",
-  "type": "Section",
-  "number": "1.3",
-  "title": "Stradella Bass Keyboard Layout",
-  "body": " Stradella Bass Keyboard Layout  This section will cover the layout of the Stradella bass keyboard and its use in chord and scale construction.   Stradella Bass Keyboard. Vertical lines represent Bellows      "
-},
-{
-  "id": "stradella",
-  "level": "2",
-  "url": "sec-stradella-bass-keyboard-layout.html#stradella",
-  "type": "Figure",
-  "number": "1.3.1",
-  "title": "",
-  "body": " Stradella Bass Keyboard. Vertical lines represent Bellows     "
-},
-{
-  "id": "sec-iso-grid-intro",
-  "level": "1",
-  "url": "sec-iso-grid-intro.html",
-  "type": "Section",
-  "number": "2.1",
-  "title": "Treble Keyboard Note Layout",
-  "body": " Treble Keyboard Note Layout  An isometric grid visualization created using TiKZ.   Treble Keyboard Note Layout      "
-},
-{
-  "id": "fig-iso-grid",
-  "level": "2",
-  "url": "sec-iso-grid-intro.html#fig-iso-grid",
-  "type": "Figure",
-  "number": "2.1.1",
-  "title": "",
-  "body": " Treble Keyboard Note Layout     "
-},
-{
-  "id": "sec-chord-builds-intro",
-  "level": "1",
-  "url": "sec-chord-builds-intro.html",
-  "type": "Section",
-  "number": "3.1",
-  "title": "Chord Builds",
-  "body": " Chord Builds  An isometric grid visualization created using TiKZ.   Chord Builds      "
-},
-{
-  "id": "fig-chord-builds",
-  "level": "2",
-  "url": "sec-chord-builds-intro.html#fig-chord-builds",
-  "type": "Figure",
-  "number": "3.1.1",
-  "title": "",
-  "body": " Chord Builds     "
-},
-{
-  "id": "sec-Cmaj-allinversions-intro",
-  "level": "1",
-  "url": "sec-Cmaj-allinversions-intro.html",
-  "type": "Section",
-  "number": "4.1",
-  "title": "C Major Chords",
-  "body": " C Major Chords  An isometric grid visualization created using TiKZ.   C Major Chords      "
-},
-{
-  "id": "fig-Cmaj-allinversions",
-  "level": "2",
-  "url": "sec-Cmaj-allinversions-intro.html#fig-Cmaj-allinversions",
-  "type": "Figure",
-  "number": "4.1.1",
-  "title": "",
-  "body": " C Major Chords     "
+  "url": "ch-chapter-title.html",
+  "type": "Chapter",
+  "number": "1",
+  "title": "About This Document",
+  "body": " About This Document   The material in this document is written for the C System Chromatic Button Accordion with both Stradella and Free Bass.  It is not an Accordion Tutor but covers material that I have found useful in learning about the Accordion after many years of playing the Guitar.  Where I have covered a topic I have attempted to include as much information as is needed to assist someone new to the Accordion to understand the topic.  The Manual will focus primarily on Chord and Scale construction on both Treble and Free Bass keyboards.  I was drawn to the button accordion, rather than the piano accordion, noting that Chord and Scale construction on this type of keyboard allow the use of moveable shapes specific to different Chord and Scale Types. Each shape may be moved anywhere on the keyboard giving a chord or scale identified by the Tonic in the shape.  Thus knowledge of any single shape enables the player to generate the chord type represented by that shape all over the keyboard in all keys.  This use of keyboard shapes is similar to the way in which different Chords and Scales are represented (Tab) in many resources used by Guitar Players; removing the need to read music whilst still being able to learn about Scale and Chord construction.  The shapes shown for the treble keyboard are mirrored on the free-bass keyboard so it is easy to generate the chord shapes for the bass once you have grasped how the the treble side works.  As a matter of interest, on the button accordion, chord shapes are consistent across the keyboard, unlike on the guitar where the interval between strings alters between the third and second string which necessitates some alteration of chord shapes in that area of the fingerboard.  The main scales and chord types (in various inversions) will be considered.   "
 },
 {
   "id": "backmatter-2",
