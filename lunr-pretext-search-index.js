59494c3cfa1522db72d6b16727079036e45d1b61
PeterWilkens <p.j.wilkens@btinternet.com>
2026-03-09 16:10:48 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -16,14 +16,32 @@ var ptx_lunr_docs = [
   "type": "Chapter",
   "number": "1",
   "title": "About This Document",
-  "body": " About This Document   The material in this document is written for the C System Chromatic Button Accordion with both Stradella and Free Bass.  It is not an Accordion Tutor but covers material that I have found useful in learning about the Accordion after many years of playing the Guitar.  Where I have covered a topic I have attempted to include as much information as is needed to assist someone new to the Accordion to understand the topic.  The Manual will focus primarily on Chord and Scale construction on both Treble and Free Bass keyboards.  I was drawn to the button accordion, rather than the piano accordion, noting that Chord and Scale construction on this type of keyboard allow the use of moveable shapes specific to different Chord and Scale Types. Each shape may be moved anywhere on the keyboard giving a chord or scale identified by the Tonic in the shape.  Thus knowledge of any single shape enables the player to generate the chord type represented by that shape all over the keyboard in all keys.  This use of keyboard shapes is similar to the way in which different Chords and Scales are represented (Tab) in many resources used by Guitar Players; removing the need to read music whilst still being able to learn about Scale and Chord construction.  The shapes shown for the treble keyboard are mirrored on the free-bass keyboard so it is easy to generate the chord shapes for the bass once you have grasped how the the treble side works.  As a matter of interest, on the button accordion, chord shapes are consistent across the keyboard, unlike on the guitar where the interval between strings alters between the third and second string which necessitates some alteration of chord shapes in that area of the fingerboard.  The main scales and chord types (in various inversions) will be considered.   "
+  "body": " About This Document   The material in this document is written for the C System Chromatic Button Accordion with both Stradella and Free Bass.  It is not an Accordion Tutor but covers material that I have found useful in learning about the Accordion after many years of playing the Guitar.  Where I have covered a topic I have attempted to include as much information as is needed to assist someone new to this type of Accordion to understand the topic.  The Manual will focus primarily on Chord and Scale construction on both Treble and Free Bass keyboards.  I was drawn to the button accordion, rather than the piano accordion, noting that Chord and Scale construction on this type of keyboard allow the use of moveable shapes specific to different Chord and Scale Types. Each shape may be moved anywhere on the keyboard giving a chord or scale identified by the Tonic in the shape.  Thus knowledge of any single shape enables the player to generate the chord type represented by that shape all over the keyboard in all keys.  This use of keyboard shapes is similar to the way in which different Chords and Scales are represented in many resources used by Guitar Players; removing the need to read music whilst still being able to learn about Scale and Chord construction.  The shapes shown for the treble keyboard are mirrored on the free-bass keyboard so it is easy to generate the chord shapes for the bass once you have grasped how the the treble side works.  As a matter of interest, on the button accordion, chord shapes are consistent across the keyboard, unlike on the guitar where the interval between strings alters between the third and second string which necessitates some alteration of chord shapes in that area of the fingerboard.  The main scales and chord types (in various inversions) will be considered.    "
+},
+{
+  "id": "sec-chromatic-button-accordion-intro",
+  "level": "1",
+  "url": "sec-chromatic-button-accordion-intro.html",
+  "type": "Section",
+  "number": "2.1",
+  "title": "A Chromatic Button Accordion",
+  "body": " A Chromatic Button Accordion  The chromatic button accordion is a versatile instrument, distinguished by its button-based keyboard layout and rich tonal possibilities. This section provides an overview of a C system instrument with Stradella and Free Bass systems.   High quality Italian Chromatic Button Accordion with Stradella and Free Bass.   High Quality Italian Chromatic Button Accordion with Free Bass and Stradella. The treble side keyboard is configured in the C System.    "
+},
+{
+  "id": "master-58-b-de-luxe",
+  "level": "2",
+  "url": "sec-chromatic-button-accordion-intro.html#master-58-b-de-luxe",
+  "type": "Figure",
+  "number": "2.1.1",
+  "title": "",
+  "body": " High quality Italian Chromatic Button Accordion with Stradella and Free Bass.   High Quality Italian Chromatic Button Accordion with Free Bass and Stradella. The treble side keyboard is configured in the C System.   "
 },
 {
   "id": "sec-section-name",
   "level": "1",
   "url": "sec-section-name.html",
   "type": "Section",
-  "number": "2.1",
+  "number": "2.2",
   "title": "C System Treble Keyboard Layout Diagram",
   "body": " C System Treble Keyboard Layout Diagram   Treble Side Keyboard. Vertical lines represent Bellows      "
 },
@@ -32,7 +50,7 @@ var ptx_lunr_docs = [
   "level": "2",
   "url": "sec-section-name.html#tikz-example",
   "type": "Figure",
-  "number": "2.1.1",
+  "number": "2.2.1",
   "title": "",
   "body": " Treble Side Keyboard. Vertical lines represent Bellows     "
 },
@@ -41,7 +59,7 @@ var ptx_lunr_docs = [
   "level": "1",
   "url": "sec-free-bass-keyboard-layout.html",
   "type": "Section",
-  "number": "2.2",
+  "number": "2.3",
   "title": "C System Free Bass Keyboard Layout Diagram",
   "body": " C System Free Bass Keyboard Layout Diagram   Free Bass Keyboard. Vertical lines represent Bellows      "
 },
@@ -50,7 +68,7 @@ var ptx_lunr_docs = [
   "level": "2",
   "url": "sec-free-bass-keyboard-layout.html#freebass",
   "type": "Figure",
-  "number": "2.2.1",
+  "number": "2.3.1",
   "title": "",
   "body": " Free Bass Keyboard. Vertical lines represent Bellows     "
 },
@@ -59,7 +77,7 @@ var ptx_lunr_docs = [
   "level": "1",
   "url": "sec-stradella-bass-keyboard-layout.html",
   "type": "Section",
-  "number": "2.3",
+  "number": "2.4",
   "title": "Stradella Bass Keyboard Layout",
   "body": " Stradella Bass Keyboard Layout  This section will cover the layout of the Stradella bass keyboard and its use in chord and scale construction.   Stradella Bass Keyboard. Vertical lines represent Bellows      "
 },
@@ -68,7 +86,7 @@ var ptx_lunr_docs = [
   "level": "2",
   "url": "sec-stradella-bass-keyboard-layout.html#stradella",
   "type": "Figure",
-  "number": "2.3.1",
+  "number": "2.4.1",
   "title": "",
   "body": " Stradella Bass Keyboard. Vertical lines represent Bellows     "
 },
@@ -115,7 +133,7 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "5.1",
   "title": "C Major Chords",
-  "body": " C Major Chords  All notes of CMajor Chord (C,E and G shown on grid allowing visualisation of Root, 1st and 2nd inversion triads).   C Major Chords      "
+  "body": " C Major Chords  All the notes of a C Major Chord (C,E and G shown on a grid allowing visualisation of Root, 1st and 2nd inversion triads).   C Major Chords      "
 },
 {
   "id": "fig-Cmaj-build",
