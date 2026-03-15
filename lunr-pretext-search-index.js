d62755d7a891da86bcf5610363dca6deaa02cf2c
PeterWilkens <p.j.wilkens@btinternet.com>
2026-03-15 16:49:24 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -91,11 +91,164 @@ var ptx_lunr_docs = [
   "body": " Stradella Bass Keyboard. Vertical lines represent Bellows     "
 },
 {
+  "id": "sec-intervals",
+  "level": "1",
+  "url": "sec-intervals.html",
+  "type": "Section",
+  "number": "3.1",
+  "title": "Intervals",
+  "body": " Intervals   Before discussing scales it is useful to understand how the distance between notes is described. The musical distance between notes on your instrument keyboard or on a musical staff is known as an interval . This interval is dependant on the number of semitones between notes and the Interval is described as a minor 2nd, a major 2nd, a minor 3rd, a major 3rd, a 5th, a 7th etc;  The interval between two notes can be obtained by counting between notes on the keyboard or staff but, when counting, the first note, should be counted as one and not zero. Using this type of count the actual number of semitones is 1 less than the count. Considering the C Major scale in Figure 3.1.1 the interval C to D is a Major second because there are 2 semitones between C and D. The Count is 3. That is, C to C :1 semitone. C to D:1 semitone. Count C is 1, C is 2 and D is 3.  Similarly E to F is a Minor Second. One semitone between E and F and a count of 2. Count 1 at E and 2 at F.   "
+},
+{
+  "id": "sec-intervals-2-1",
+  "level": "2",
+  "url": "sec-intervals.html#sec-intervals-2-1",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "interval interval Interval "
+},
+{
+  "id": "sec-intervals-2-2",
+  "level": "2",
+  "url": "sec-intervals.html#sec-intervals-2-2",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "interval interval Count Count "
+},
+{
+  "id": "sec-intervals-c-major-scale",
+  "level": "1",
+  "url": "sec-intervals-c-major-scale.html",
+  "type": "Section",
+  "number": "4.1",
+  "title": "C Major Scale",
+  "body": " C Major Scale  In C major, the interval pattern remains the same ascending and descending.  All major scales have the same pattern of intervals, so any major scale can be built by using this same pattern.  The C major scale notes are C, D, E, F, G, A, B, and C.   C major scale notation on a musical staff (ascending and descending).       Examples of intervals with notation and semitone counts (C major scale).      "
+},
+{
+  "id": "fig-c-major-scale-notation",
+  "level": "2",
+  "url": "sec-intervals-c-major-scale.html#fig-c-major-scale-notation",
+  "type": "Figure",
+  "number": "4.1.1",
+  "title": "",
+  "body": " C major scale notation on a musical staff (ascending and descending).     "
+},
+{
+  "id": "fig-intervals-c-major-scale-table",
+  "level": "2",
+  "url": "sec-intervals-c-major-scale.html#fig-intervals-c-major-scale-table",
+  "type": "Figure",
+  "number": "4.1.2",
+  "title": "",
+  "body": " Examples of intervals with notation and semitone counts (C major scale).     "
+},
+{
+  "id": "sec-intervals-c-natural-minor",
+  "level": "1",
+  "url": "sec-intervals-c-natural-minor.html",
+  "type": "Section",
+  "number": "4.2",
+  "title": "C Natural Minor Scale",
+  "body": " C Natural Minor Scale  In C natural minor, the interval pattern differs from the major scale in that the third, sixth, and seventh scale degrees are lowered.  All natural minor scales follow the same interval pattern, so any natural minor scale can be built by using this pattern of major and minor seconds.  The C natural minor scale notes are C, D, E , F, G, A , B , and C.  A natural minor scale is the relative minor of a major scale. The A minor natural scale uses these same notes, the sixth note of the major scale becoming the root note of the relative minor. ie.) the notes of the A Minor Natural Scale are A, B, C, D, E, F, G and A.  C minor natural is the relative minor of E major.   C natural minor scale notation on a musical staff (ascending and descending).       Examples of intervals with notation and semitone counts (C natural minor scale ascending).      "
+},
+{
+  "id": "fig-c-natural-minor-scale-notation",
+  "level": "2",
+  "url": "sec-intervals-c-natural-minor.html#fig-c-natural-minor-scale-notation",
+  "type": "Figure",
+  "number": "4.2.1",
+  "title": "",
+  "body": " C natural minor scale notation on a musical staff (ascending and descending).     "
+},
+{
+  "id": "fig-intervals-c-natural-minor-ascending-table",
+  "level": "2",
+  "url": "sec-intervals-c-natural-minor.html#fig-intervals-c-natural-minor-ascending-table",
+  "type": "Figure",
+  "number": "4.2.2",
+  "title": "",
+  "body": " Examples of intervals with notation and semitone counts (C natural minor scale ascending).     "
+},
+{
+  "id": "sec-intervals-c-harmonic-minor-scale",
+  "level": "1",
+  "url": "sec-intervals-c-harmonic-minor-scale.html",
+  "type": "Section",
+  "number": "4.3",
+  "title": "C Harmonic Minor Scale",
+  "body": " C Harmonic Minor Scale  In C harmonic minor, the interval pattern remains the same ascending and descending.  All harmonic minor scales have the same pattern of intervals, so any harmonic minor scale can be built by using this same pattern.  The C harmonic minor scale notes are C, D, E , F, G, A , B, and C. In this scale, the interval from A to B is an augmented second, spanning three semitones while still being spelled as a second.    Examples of intervals with notation and semitone counts (C harmonic minor scale).      "
+},
+{
+  "id": "fig-intervals-c-harmonic-minor-scale-table",
+  "level": "2",
+  "url": "sec-intervals-c-harmonic-minor-scale.html#fig-intervals-c-harmonic-minor-scale-table",
+  "type": "Figure",
+  "number": "4.3.1",
+  "title": "",
+  "body": " Examples of intervals with notation and semitone counts (C harmonic minor scale).     "
+},
+{
+  "id": "sec-intervals-c-melodic-minor-ascending",
+  "level": "1",
+  "url": "sec-intervals-c-melodic-minor-ascending.html",
+  "type": "Section",
+  "number": "4.4",
+  "title": "C Melodic Minor Scale (Ascending)",
+  "body": " C Melodic Minor Scale (Ascending)  In C melodic minor ascending, the sixth and seventh scale degrees are raised to A and B.  All melodic minor scales use this same ascending interval pattern, so any melodic minor scale can be written ascending by applying this pattern from tonic to tonic.  The C melodic minor ascending scale notes are C, D, E , F, G, A, B, and C.   C melodic minor scale notation on a musical staff (ascending and descending).       Examples of intervals with notation and semitone counts (C melodic minor scale ascending).      "
+},
+{
+  "id": "fig-c-melodic-minor-scale-notation",
+  "level": "2",
+  "url": "sec-intervals-c-melodic-minor-ascending.html#fig-c-melodic-minor-scale-notation",
+  "type": "Figure",
+  "number": "4.4.1",
+  "title": "",
+  "body": " C melodic minor scale notation on a musical staff (ascending and descending).     "
+},
+{
+  "id": "fig-intervals-c-melodic-minor-ascending-table",
+  "level": "2",
+  "url": "sec-intervals-c-melodic-minor-ascending.html#fig-intervals-c-melodic-minor-ascending-table",
+  "type": "Figure",
+  "number": "4.4.2",
+  "title": "",
+  "body": " Examples of intervals with notation and semitone counts (C melodic minor scale ascending).     "
+},
+{
+  "id": "sec-intervals-c-melodic-minor-descending",
+  "level": "1",
+  "url": "sec-intervals-c-melodic-minor-descending.html",
+  "type": "Section",
+  "number": "4.5",
+  "title": "C Melodic Minor Scale (Descending)",
+  "body": " C Melodic Minor Scale (Descending)  In C melodic minor descending, the sixth and seventh scale degrees are lowered to A and B , following the C natural minor form.  All melodic minor scales use this same descending interval pattern, so any melodic minor scale can be written descending by applying this pattern from tonic to tonic.  The C melodic minor descending scale notes are C, B , A , G, F, E , D, and C.   C melodic minor scale notation on a musical staff (ascending and descending).       Examples of intervals with notation and semitone counts (C melodic minor scale descending).      "
+},
+{
+  "id": "fig-c-melodic-minor-descending-scale-notation",
+  "level": "2",
+  "url": "sec-intervals-c-melodic-minor-descending.html#fig-c-melodic-minor-descending-scale-notation",
+  "type": "Figure",
+  "number": "4.5.1",
+  "title": "",
+  "body": " C melodic minor scale notation on a musical staff (ascending and descending).     "
+},
+{
+  "id": "fig-intervals-c-melodic-minor-descending-table",
+  "level": "2",
+  "url": "sec-intervals-c-melodic-minor-descending.html#fig-intervals-c-melodic-minor-descending-table",
+  "type": "Figure",
+  "number": "4.5.2",
+  "title": "",
+  "body": " Examples of intervals with notation and semitone counts (C melodic minor scale descending).     "
+},
+{
   "id": "sec-iso-grid-intro",
   "level": "1",
   "url": "sec-iso-grid-intro.html",
   "type": "Section",
-  "number": "3.1",
+  "number": "5.1",
   "title": "Treble Keyboard Note Layout",
   "body": " Treble Keyboard Note Layout  An isometric grid visualization created using TiKZ.   Treble Keyboard Note Layout      "
 },
@@ -104,7 +257,7 @@ var ptx_lunr_docs = [
   "level": "2",
   "url": "sec-iso-grid-intro.html#fig-iso-grid",
   "type": "Figure",
-  "number": "3.1.1",
+  "number": "5.1.1",
   "title": "",
   "body": " Treble Keyboard Note Layout     "
 },
@@ -113,7 +266,7 @@ var ptx_lunr_docs = [
   "level": "1",
   "url": "sec-chord-builds.html",
   "type": "Section",
-  "number": "4.1",
+  "number": "6.1",
   "title": "Chord Builds",
   "body": " Chord Builds  An isometric grid visualization created using TiKZ.   Chord Builds      "
 },
@@ -122,7 +275,7 @@ var ptx_lunr_docs = [
   "level": "2",
   "url": "sec-chord-builds.html#fig-chord-builds",
   "type": "Figure",
-  "number": "4.1.1",
+  "number": "6.1.1",
   "title": "",
   "body": " Chord Builds     "
 },
@@ -131,7 +284,7 @@ var ptx_lunr_docs = [
   "level": "1",
   "url": "sec-Cmaj-build.html",
   "type": "Section",
-  "number": "5.1",
+  "number": "7.1",
   "title": "C Major Chords",
   "body": " C Major Chords  All the notes of a C Major Chord (C,E and G shown on a grid allowing visualisation of Root, 1st and 2nd inversion triads).   C Major Chords      "
 },
@@ -140,7 +293,7 @@ var ptx_lunr_docs = [
   "level": "2",
   "url": "sec-Cmaj-build.html#fig-Cmaj-build",
   "type": "Figure",
-  "number": "5.1.1",
+  "number": "7.1.1",
   "title": "",
   "body": " C Major Chords     "
 },
